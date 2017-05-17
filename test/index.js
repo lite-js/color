82a55711ea5a02a74dacf9a9e4c18c11c8b489ff
leungwensen <leungwensen@gmail.com>
2017-05-17 18:38:34 +0800

    init

---
+++ test/index.js
@@ -0,0 +1,42 @@
+const assert = require('assert');
+const Color = require('../src/index')
+
+describe('Color', () => {
+  it('Color initProps', () => {
+    const color = new Color()
+    assert.equal(color.r, 255)
+    assert.equal(color.g, 255)
+    assert.equal(color.b, 255)
+    assert.equal(color.a, 1)
+  })
+  it('white is #fff', () => {
+    assert.deepEqual(
+      Color.fromString('white').toRgba(),
+      new Color('#fff').toRgba()
+    )
+  })
+  it('white is also #ffffff', () => {
+    assert.deepEqual(
+      Color.fromString('white').toRgba(),
+      new Color('#ffffff').toRgba()
+    )
+  })
+  it('white is new Color()', () => {
+    assert.deepEqual(
+      Color.fromString('white').toRgba(),
+      new Color().toRgba()
+    )
+  })
+  it('maroon is #800000', () => {
+    assert.deepEqual(
+      Color.fromString('maroon').toRgba(),
+      new Color('#800000').toRgba()
+    )
+  })
+  it('toGrey returns instance of Color', () => {
+    assert.equal(
+      Color.fromString('maroon').toGrey() instanceof Color,
+      true
+    )
+  })
+})
